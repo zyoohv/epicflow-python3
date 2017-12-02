42c093707fc753b745fce55e5b5ea1e80edfda0a
zyoohv <zyoohv@YonghuiZhang>
2017-12-02 15:13:36 +0800

    fix readme

--- example.py
+++ example.py
@@ -3,4 +3,4 @@ from lib import get_epicflow
 task_list = [['../1.png', '../2.png', '../av.png'], ['../1.png', '../2.png', '../ava.png'], 
     ['../1.png', '../2.png', '../avav.png'], ['../1.png', '../2.png', '../avava.png']]
 
-get_epicflow(task_list=task_list, thread_num=2, lib_path='./lib')
+get_epicflow(task_list=task_list, thread_num=3, lib_path='./lib')
