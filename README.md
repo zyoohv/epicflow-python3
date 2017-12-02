c8bb22f93bc63bea0e359751e887e03d23466db9
zyoohv <zyoohv@YonghuiZhang>
2017-12-02 15:11:29 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# epicflow-python3
-Abstract Epicflow with python3
+# epicflow-python2/3
+
+Abstract Epicflow with python2/3
+
+
+## 1. Compile
+
+*   compile SED_edge
+
+```
+$ cd ./lib/SED_edge
+$ mex private/edgesDetectMex.cpp -outdir private
+$ mex private/edgesNmsMex.cpp    -outdir private
+$ mex private/spDetectMex.cpp    -outdir private
+$ mex private/edgeBoxesMex.cpp   -outdir private
+```
+
+*   compile flow-code
+
+```
+$ cd ./lib/flow-code
+$ cd imageLib
+$ make
+$ cd ..
+$ make
+```
+
+## 2. Run example
+
+```
+$ python3 example.py
+```
+
+
+## 3. Interface
+
+more information reference file 'example.py' please.
---
+++ lib/__init__.py
@@ -0,0 +1,82 @@
+import os
+import threading
+import time
+
+lock = threading.RLock()
+
+task_idx = 0
+task_tot = 0
+task_list = []
+lib_path = ""
+
+
+def transtoflo(thread_name):
+    global task_idx
+    trans_cmd = 'cd {}; matlab -nodisplay -r \"'.format(lib_path)
+    while True:
+        lock.acquire()
+        if task_idx >= task_tot:
+            lock.release()
+            break
+        img1_path, img2_path, out_path = task_list[task_idx]
+        task_idx += 1
+        lock.release()
+        trans_cmd += 'get_epicflow(\'{}\', \'{}\', \'{}.flo\'); '.format(img1_path, img2_path, out_path[:-4])
+    trans_cmd += 'exit;\"'
+    print('trans_cmd=', trans_cmd)
+    os.system(trans_cmd)
+
+
+def transtopng(thread_name):
+    global task_idx
+    while True:
+        lock.acquire()
+        if task_idx >= task_tot:
+            lock.release()
+            break
+        _, _, out_path = task_list[task_idx]
+        task_idx += 1
+        lock.release()
+
+        transcmd = 'cd {}; flow-code/color_flow {}.flo {}'.format(lib_path, out_path[:-4], out_path)
+        os.system(transcmd)
+        delcmd = 'cd {}; rm {}.flo'.format(lib_path, out_path[:-4])
+        os.system(delcmd)
+
+
+def get_epicflow(**args):
+    '''
+    task_list=[]
+    thread_num = 5
+    lib_path = ./lib
+    '''
+    global task_list
+    global task_tot
+    global lib_path
+    global task_idx
+
+    task_list = args['task_list']
+    task_tot = len(task_list)
+    lib_path = args['lib_path']
+
+    thread_num = args['thread_num']
+
+    task_idx = 0
+    transtoflo_list = []
+    for thread_name in range(thread_num):
+        transtoflo_list.append(threading.Thread(target=transtoflo, args=(thread_name,)))
+        transtoflo_list[-1].start()
+    for thread in transtoflo_list:
+        thread.join()
+
+    print('hahahahaha!!!!!!!!!!!!!!!!!!')
+    time.sleep(5)
+
+    task_idx = 0
+    transtopng_list = []
+    for thread_name in range(thread_num):
+        transtopng_list.append(threading.Thread(target=transtopng, args=(thread_name,)))
+        transtopng_list[-1].start()
+    for thread in transtopng_list:
+        thread.join()
+
